aa39a66b4120071b4747d513fd04fbf33bcc4096
Miles Johnson <mileswjohnson@gmail.com>
2012-03-14 23:41:23 -0700

    Updated constants and paths

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 
 //declare(encoding='UTF-8');
 
-use \titon\Titon;
-
 /**
  * Compare the PHP version so that the application is running in 5.3!
  */
@@ -21,21 +19,29 @@ if (version_compare(PHP_VERSION, '5.3.0') == -1) {
 /**
  * Convenience constants for the directory, path and namespace separators.
  */
-define('DS', '/');
-define('PS', PATH_SEPARATOR);
-define('NS', '\\');
+if (!defined('DS')) {
+	define('DS', '/');
+}
+
+if (!defined('PS')) {
+	define('PS', PATH_SEPARATOR);
+}
+
+if (!defined('NS')) {
+	define('NS', '\\');
+}
 
 /**
  * Define the folders that contain the app and Titon files.
  */
-define('APP', str_replace(NS, DS, __DIR__) . DS);
-define('ROOT', dirname(APP) . DS);
-define('TITON', ROOT . 'titon' . DS);
+define('TITON_APP', __DIR__ . DS);
+define('TITON_VENDORS', dirname(__DIR__) . DS . 'titon' . DS . 'vendors' . DS);
+define('TITON_SOURCE', TITON_VENDORS . 'titon' . DS);
 
 /**
  * Load the core Titon files and initialize dispatcher; throw fatal error if libraries could not be found.
  */
-$bootstrap = TITON . 'bootstrap.php';
+$bootstrap = TITON_SOURCE . 'bootstrap.php';
 
 if (!file_exists($bootstrap)) {
 	trigger_error('Titon: Framework bootstrap failed to load the core libraries. Please check your paths and configuration.', E_USER_ERROR);
@@ -46,4 +52,4 @@ include_once $bootstrap;
 /**
  * Dispatch the framework.
  */
-Titon::run();
+\titon\Titon::run();
